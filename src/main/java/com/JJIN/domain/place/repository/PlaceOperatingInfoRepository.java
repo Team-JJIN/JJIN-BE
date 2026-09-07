101aa93251169c3a71c0b56a595681e597b2e03f
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-09-07 19:42:21 +0900

    feat: TourAPI 장소 DB 동기화 추가

---
+++ src/main/java/com/JJIN/domain/place/repository/PlaceOperatingInfoRepository.java
@@ -0,0 +1,8 @@
+package com.JJIN.domain.place.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.JJIN.domain.place.entity.PlaceOperatingInfo;
+
+public interface PlaceOperatingInfoRepository extends JpaRepository<PlaceOperatingInfo, Long> {
+}
